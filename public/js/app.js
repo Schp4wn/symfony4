bdea52a934f508b1afc160bc1c539e7e1d1cb650
Schp4wn <schp4wn@gmail.com>
2018-02-08 16:58:20 +0100

    doctrine

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,14 @@
-// assets/js/app.js
-require('assets/css/app.scss');
-
-// ...rest of JavaScript code here
+(function ($) {
+    $.fn.flexVerticalCenter = function (onAttribute) {
+        return this.each(function () {
+            var $this = $(this);
+            var attribute = onAttribute || 'margin-top';
+            var resizer = function () {
+                $this.css(attribute, (($this.parent().height() - $this.height()) / 2));
+            };
+            resizer();
+            $(window).resize(resizer);
+            $this.find('img').load(resizer);
+        });
+    };
+})(jQuery);
